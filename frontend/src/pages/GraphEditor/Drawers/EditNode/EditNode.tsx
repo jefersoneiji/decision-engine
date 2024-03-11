ffbee66a5f9e74dc9156c2fbf837f6ec5cbbd6c3
jefersoneiji <jeferson.eiji@icloud.com>
2024-03-11 17:18:20 -0300

    feat (updateNode)!: logic for updating node was added, including a drawer for such purpose

---
+++ frontend/src/pages/GraphEditor/Drawers/EditNode/EditNode.tsx
@@ -0,0 +1,78 @@
+import { Drawer } from 'components/Drawer'
+import { useContext, useState } from 'react'
+import { editor } from '@src/pages/GraphEditor/Editor'
+import type { CommonDrawerProps } from '..';
+import { graph } from '../../Graph';
+
+type EditNodeDrawerProps = {
+    operation: string;
+    value: number;
+    parameter: string;
+    label: string;
+} & CommonDrawerProps
+
+export const EditNodeDrawer = ({ id: nodeId, operation, parameter, value }: EditNodeDrawerProps) => {
+    const { drawerVisible, closeEditorDrawer } = useContext(editor)
+    const { updateNode } = useContext(graph)
+
+    const [currOperation, setOperation] = useState(operation || '>')
+    const [currParameter, setParameter] = useState(parameter || '')
+    const [currValue, setValue] = useState(value || 0)
+
+    const onSave = () => {
+        const label = currParameter + ' ' + currOperation + " " + currValue
+        updateNode({
+            id: nodeId!,
+            operation: currOperation,
+            parameter: currParameter,
+            value: currValue,
+            label
+        })
+    }
+
+    return (
+        <Drawer
+            title="Edit Node"
+            content={
+                <>
+                    <span className='font-sans'>IF </span>
+                    <input
+                        type='text'
+                        className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                        value={currParameter}
+                        onChange={e => setParameter(e.target.value)}
+                        style={{ maxWidth: 100 }}
+                    />
+                    {' '}
+                    <select
+                        name='operators'
+                        className='shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                        value={currOperation}
+                        onChange={(e) => setOperation(e.target.value)}
+                    >
+                        <option value=">">{'>'}</option>
+                        <option value=">=">{'>='}</option>
+                        <option value="=">{'='}</option>
+                        <option value="<">{'<'}</option>
+                        <option value="<=">{'<='}</option>
+                    </select>
+                    {' '}
+                    <input
+                        type='text'
+                        className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                        value={currValue}
+                        onChange={e => setValue(Number(e.target.value))}
+                        style={{ maxWidth: 100 }}
+                    />
+                    <br />
+                    <br />
+                    <button onClick={onSave} className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>
+                        Save
+                    </button>
+                </>
+            }
+            onClose={closeEditorDrawer}
+            visible={drawerVisible}
+        />
+    )
+}
--- frontend/src/pages/GraphEditor/Drawers/index.tsx
+++ frontend/src/pages/GraphEditor/Drawers/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 
 import { DrawerName, editor } from '../Editor';
 import { ChooseNodeDrawer } from './ChooseNode/ChooseNode';
+import { EditNodeDrawer } from './EditNode/EditNode';
 
 export type CommonDrawerProps = {
   id?: string;
@@ -9,7 +10,8 @@ export type CommonDrawerProps = {
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const drawers = {
-  [DrawerName.newNode]: ChooseNodeDrawer
+  [DrawerName.newNode]: ChooseNodeDrawer,
+  [DrawerName.editNode]: EditNodeDrawer
 } satisfies Record<DrawerName, (props: never) => JSX.Element>;
 
 export const CurrentDrawer = () => {
