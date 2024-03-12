0a13f60c4e55566a35c101b089d133af709c12f6
jefersoneiji <jeferson.eiji@icloud.com>
2024-03-12 13:47:15 -0300

    feat (database)!: created first routes in flask that execute db operations and did database setup

---
+++ backend/models/policy.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from sqlalchemy import Column, Integer, String, JSON, DateTime
+from sqlalchemy.sql import func
+
+from . import Base
+
+@dataclass
+class Policy(Base):
+    __tablename__='policies'
+    id: int = Column(Integer, primary_key=True)
+    title: str = Column(String)
+    edges: str = Column(JSON)
+    nodes: str = Column(JSON)
+    createdAt: DateTime = Column(DateTime(timezone=True), server_default=func.now())
+
+class Edge: 
+    id: str
+    source: str
+    target: str
+    label: str
+    sourceHandle: str
+
+class NodeData: 
+    parameter: str
+    operation: str
+    value: str
+
+class Node: 
+    id: str
+    data: NodeData
+    type: str
