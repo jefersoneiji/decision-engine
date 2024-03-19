8863d482e77893193d940f2a9560b23b7dd6cb7a
jefersoneiji <jeferson.eiji@icloud.com>
2024-03-19 14:51:43 -0300

    test (policy)!: update policy route added to back-end

--- backend/tests/test_policy_utils.py
+++ backend/tests/test_policy_utils.py
@@ -1,11 +1,15 @@
 import json
+from typing import Any, Callable, Optional
 
 def load_mock_data(file_path):
     with open(file_path) as file:
         return json.load(file)
 
-def generate_policy(policy):
+def generate_policy(policy, modifications=None):
     data = load_mock_data('./tests/mockData.json')[policy]
+    if modifications is not None:
+        for key in modifications:
+            data[key] = modifications[key]
     return data
 
 def create_policy(client, policy='simple'):
@@ -21,7 +25,7 @@ def contains(value):
                 assert data[key] == value[key]
     return contains_data
 
-def check_response(response,status=None, length=None, data=None):
+def check_response(response,status=None, length=None, data=None, contains:Optional[Callable[[Any], None]] = None):
     response_data = response.get_json()
     if status is not None:
         assert response.status_code == status
@@ -31,6 +35,11 @@ def check_response(response,status=None, length=None, data=None):
         assert response_data == data
         if length != None:
             assert len(response_data) == length
+    if contains is not None:
+        contains(data=response_data)
 
 def delete_policy(client, id):
     return client.delete(f'/policies/{id}')
+
+def update_policy(client, id, policy="simple", modifications=None):
+    return client.put(f'/policies/{id}',json=generate_policy(policy, modifications))
