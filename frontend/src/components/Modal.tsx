4d60af07525c9c341ddad0ad27510544412af3ea
jefersoneiji <jeferson.eiji@icloud.com>
2024-03-19 10:25:08 -0300

    feat (modal)!: create modal basic elements for GraphEditor

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -9,13 +9,17 @@ export const Modal: FC<Modal & PropsWithChildren> = ({ open, onClose, children }
         <div className={`fixed inset-0 flex justify-center items-center transition-colors ${open ? "visible bg-black/20" : "invisible"}`}
             onClick={onClose}
         >
-            <div className={`bg-white rounded-xl shadow p-6 transition-all ${open ? "scale-100 opacity-100" : "scale-12 opacity-0"}`}
+            <div className={`h-full max-h-64 w-full max-w-[40rem] bg-white rounded-xl shadow p-6 transition-all ${open ? "scale-100 opacity-100" : "scale-12 opacity-0"}`}
                 onClick={e => e.stopPropagation()}
             >
-                <button onClick={onClose} className="absolute top-2 right-2 p-1 rounded-lg text-gray-400 bg-white hover:bg-gray-50 hover:text-gray-600">
-                    X
-                </button>
-                {children}
+                <div className="flex flex-row justify-end">
+                    <button onClick={onClose} className="mb-2 rounded-lg text-gray-400 bg-white hover:bg-gray-50 hover:text-gray-600">
+                        x
+                    </button>
+                </div>
+                <div className="max-h-48 overflow-y-scroll">
+                    {children}
+                </div>
             </div>
         </div>
     )
--- frontend/src/pages/GraphEditor/GraphEditor.tsx
+++ frontend/src/pages/GraphEditor/GraphEditor.tsx
@@ -11,7 +11,7 @@ import { GraphProvider, graph } from "./Graph";
 import { allNodes } from "./Nodes";
 import { generateEdge, generateNode } from "./nodeGeneration";
 import { positionNodes } from "./positionNodes";
-import { savePolicy } from "@src/api/policies";
+import { getAllPolicies, savePolicy } from "@src/api/policies";
 import { PolicyProvider, policy } from "./Policy";
 import { Modal } from "@src/components/Modal";
 
@@ -156,12 +156,29 @@ const EditPolicyButton = () => {
 
 const PoliciesList = () => {
   const [open, setOpen] = useState(false)
-
+  const [policies, setPolicies] = useState([])
+  
+  useEffect(() => {
+    getAllPolicies().then(res => {
+      setPolicies(res.data)
+    })
+  }, [])
   const onOpen = () => {
     setOpen(true)
   }
   const onClose = () => {
     setOpen(false)
+  }
+
+  const Row = ({ title, createdAt }: { title: string, createdAt: string }) => {
+    return (
+      <tr className="border-b border-neutral-200">
+        <td>{title}</td>
+        <td>{createdAt}</td>
+        <td><TrashIcon /></td>
+        <td><EditIcon /></td>
+      </tr>
+    )
   }
 
   return (
@@ -173,7 +190,20 @@ const PoliciesList = () => {
         Policies List
       </button>
       <Modal open={open} onClose={onClose} >
-        <p>Testing</p>
+        <table className="w-full table-auto text-center">
+          <thead className="border-b border-neutral-200">
+            <tr>
+              <th>Name</th>
+              <th>Created</th>
+              <th>Delete</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {policies.length > 0 && policies.map((policy, idx) =>
+              <Row key={idx} title={policy.title} createdAt={policy.createdAt} />)}
+          </tbody>
+        </table>
       </Modal>
     </>
   )
@@ -188,4 +218,24 @@ export function GraphEditor() {
       </GraphProvider>
     </EditorProvider>
   );
+}
+
+const TrashIcon = () => {
+  return (
+    <div className="flex justify-center cursor-pointer">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+      </svg>
+    </div>
+  )
+}
+
+const EditIcon = () => {
+  return (
+    <div className="flex justify-center cursor-pointer">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+      </svg>
+    </div>
+  )
 }
